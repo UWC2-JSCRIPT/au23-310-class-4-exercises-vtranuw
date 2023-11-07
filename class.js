aa5c9e84b34e383816ff4097257bfb7483675ac3
Viet Tran <viettran@Viets-MacBook-Pro.local>
2023-11-07 11:10:51 -0800

    Finished by Viet Tran

--- class.js
+++ class.js
@@ -8,3 +8,19 @@ const makeCoffee = (isHot) => {
   }
   return coffee
 }
+
+// Test Cases:
+// Test Case 1: Hot coffee
+const hotCoffee = makeCoffee(true);
+console.log("Hot Coffee:");
+console.log("Cup:", hotCoffee.cup); // Should be 'Styrofoam'
+console.log("Is Hot:", hotCoffee.isHot); // Should be true
+console.log("Extras:", hotCoffee.extras); // Should be an empty array
+
+// Test Case 2: Cold coffee
+const coldCoffee = makeCoffee(false);
+console.log("\nCold Coffee:");
+console.log("Cup:", coldCoffee.cup); // Should be 'paper'
+console.log("Is Hot:", coldCoffee.isHot); // Should be false
+console.log("Extras:", coldCoffee.extras); // Should contain 'ice'
+
