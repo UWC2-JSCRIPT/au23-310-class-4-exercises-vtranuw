fcd7af4190d00c6d31661bbc50d72275be893837
Viet Tran <viettran@Viets-MacBook-Pro.local>
2023-11-07 11:06:09 -0800

    First Commited by Viet T

--- createCardDeck.js
+++ createCardDeck.js
@@ -2,9 +2,9 @@
  * Returns an array of 52 Cards
  * @returns {Array} deck - a deck of cards
  */
-const getDeck = () => {
-  const deck = []
-  const suits = ['hearts', 'spades', 'clubs', 'diamonds']
+ const getDeck = () => {
+  const deck = [];
+  const suits = ["hearts", "spades", "clubs", "diamonds"];
 
   for (let index = 0; index < suits.length; index++) {
     // create an array of 13 objects
@@ -12,57 +12,64 @@ const getDeck = () => {
       // for each loop, push a card object to the deck
 
       // special cases for when j > 10
-      const displayVal = ''
+      let displayVal = "";
 
       switch (j) {
         case j === 1:
-          displayVal = 'Ace'
-          break
+          displayVal = "Ace";
+          break;
         case j > 1 && j <= 10:
-          displayVal = j
-          break
+          displayVal = j;
+          break;
         case j === 11:
-          displayVal = 'Jack'
-          break
+          displayVal = "Jack";
+          break;
         case j === 12:
-          displayVal = 'Queen'
-          break
+          displayVal = "Queen";
+          break;
         case j === 13:
-          displayVal = 'King'
-          break
+          displayVal = "King";
+          break;
+        default:
+          displayVal = j;
       }
 
       const card = {
         val: j,
         displayVal: displayVal,
         suit: suits[index],
+      };
+
+      if (displayVal === "Ace") {
+        card.val = 11;
       }
 
-      if (displayVal === 'Ace') {
-        card.val = 11
-      }
-
-      deck.push(card)
+      deck.push(card);
     }
   }
-}
+  return deck;
+};
+
+module.exports = {
+  getDeck,
+};
 
 // CHECKS
-const deck = getDeck()
-console.log(`Deck length equals 52? ${deck.length === 52}`)
+const deck = getDeck();
+console.log(`Deck length equals 52? ${deck.length === 52}`);
 
-const randomCard = deck[Math.floor(Math.random() * 52)]
+const randomCard = deck[Math.floor(Math.random() * 52)];
 
 const cardHasVal =
-  randomCard && randomCard.val && typeof randomCard.val === 'number'
-console.log(`Random card has val? ${cardHasVal}`)
+  randomCard && randomCard.val && typeof randomCard.val === "number";
+console.log(`Random card has val? ${cardHasVal}`);
 
 const cardHasSuit =
-  randomCard && randomCard.suit && typeof randomCard.suit === 'string'
-console.log(`Random card has suit? ${cardHasSuit}`)
+  randomCard && randomCard.suit && typeof randomCard.suit === "string";
+console.log(`Random card has suit? ${cardHasSuit}`);
 
 const cardHasDisplayVal =
   randomCard &&
   randomCard.displayVal &&
-  typeof randomCard.displayVal === 'string'
-console.log(`Random card has display value? ${cardHasDisplayVal}`)
+  typeof randomCard.displayVal === "string";
+console.log(`Random card has display value? ${cardHasDisplayVal}`);
--- foodIsCooked.js
+++ foodIsCooked.js
@@ -1,20 +1,30 @@
 /**
  * Determines whether meat temperature is high enough
- * @param {string} kind 
- * @param {number} internalTemp 
+ * @param {string} kind
+ * @param {number} internalTemp
  * @param {string} doneness
  * @returns {boolean} isCooked
  */
-const foodIsCooked = function(kind, internalTemp, doneness) {
-  // Write function HERE
-
-}
-
-
+ const foodIsCooked = function (kind, internalTemp, doneness) {
+  if (kind === "chicken") {
+    if (doneness === "" || internalTemp >= 165) {
+      return true;
+    }
+  } else if (kind === "beef") {
+    if (doneness === "rare" && internalTemp >= 125) {
+      return true;
+    } else if (doneness === "medium" && internalTemp >= 135) {
+      return true;
+    } else if (doneness === "well" && internalTemp >= 155) {
+      return true;
+    }
+  }
+  return false;
+};
 
 // Test function
-console.log(foodIsCooked('chicken', 90)); // should be false
-console.log(foodIsCooked('chicken', 190)); // should be true
-console.log(foodIsCooked('beef', 138, 'well')); // should be false
-console.log(foodIsCooked('beef', 138, 'medium')); // should be true
-console.log(foodIsCooked('beef', 138, 'rare')); // should be true
+console.log(foodIsCooked("chicken", 90)); // should be false
+console.log(foodIsCooked("chicken", 190)); // should be true
+console.log(foodIsCooked("beef", 138, "well")); // should be false
+console.log(foodIsCooked("beef", 138, "medium")); // should be true
+console.log(foodIsCooked("beef", 138, "rare")); // should be true
